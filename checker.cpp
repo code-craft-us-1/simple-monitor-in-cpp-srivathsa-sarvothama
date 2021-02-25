d96eea2824aa5c197dbab13ea6f1f2bf57d3b024
Sudeep <sudeep.rp@gmail.com>
2021-02-25 21:58:17 +0530

    state problem

--- checker.cpp
+++ checker.cpp
@@ -1,17 +1,22 @@
 #include <assert.h>
+#include <iostream>
+using namespace std;
 
-bool vitalsAreOk(float bpm, float spo2, float respRate) {
-  if(bpm < 70 || bpm > 150) {
+bool batteryIsOk(float temperature, float soc, float chargeRate) {
+  if(temperature < 0 || temperature > 45) {
+    cout << "Temperature out of range!\n";
     return false;
-  } else if(spo2 < 90) {
+  } else if(soc < 20 || soc > 80) {
+    cout << "State of Charge out of range!\n";
     return false;
-  } else if(respRate < 30 || respRate > 95) {
+  } else if(chargeRate > 0.8) {
+    cout << "Charge Rate out of range!\n";
     return false;
   }
   return true;
 }
 
 int main() {
-  assert(vitalsAreOk(80, 95, 60) == true);
-  assert(vitalsAreOk(60, 90, 40) == false);
+  assert(batteryIsOk(25, 70, 0.7) == true);
+  assert(batteryIsOk(50, 85, 0) == false);
 }
