4e88c23eb3187e87543b968288a2a39a514b0dcf
Sudeep Prasad <sudeep.rp@gmail.com>
2024-08-30 10:50:33 +0530

    new and improved

--- monitor.cpp
+++ monitor.cpp
@@ -1,34 +1,31 @@
-#include <iostream>
+#include "./monitor.h"
 #include <assert.h>
 #include <unistd.h>
-using namespace std;
+#include <iostream>
+using std::cout, std::flush;
 
 int vitalsOk(float temperature, float pulseRate, float spo2) {
-  if(temperature > 102 || temperature < 95) {
-    cout << "Temperature critical!\n";
-    for (int i = 0; i < 6; i++)
-    {
+  if (temperature > 102 || temperature < 95) {
+    cout << "Temperature is critical!\n";
+    for (int i = 0; i < 6; i++) {
       cout << "\r* " << flush;
       sleep(1);
       cout << "\r *" << flush;
       sleep(1);
     }
-
     return 0;
-  } else if(pulseRate < 60 || pulseRate > 100) {
+  } else if (pulseRate < 60 || pulseRate > 100) {
     cout << "Pulse Rate is out of range!\n";
-    for (int i = 0; i < 6; i++)
-    {
+    for (int i = 0; i < 6; i++) {
       cout << "\r* " << flush;
       sleep(1);
       cout << "\r *" << flush;
       sleep(1);
     }
     return 0;
-  } else if(spo2 < 90) {
+  } else if (spo2 < 90) {
     cout << "Oxygen Saturation out of range!\n";
-    for (int i = 0; i < 6; i++)
-    {
+    for (int i = 0; i < 6; i++) {
       cout << "\r* " << flush;
       sleep(1);
       cout << "\r *" << flush;
@@ -37,10 +34,4 @@ int vitalsOk(float temperature, float pulseRate, float spo2) {
     return 0;
   }
   return 1;
-}
-
-int main() {
-  assert(!vitalsOk(99, 102, 70));
-  assert(vitalsOk(98.1, 70, 98));
-  cout << "Done\n";
 }
