1984a3160a424b540e3e2ed59419ef0b52ab4458
Sarvothama, Srivathsa <srivathsa.sarvothama@philips.com>
2024-09-30 21:59:33 +0530

    Update-1

--- monitor.cpp
+++ monitor.cpp
@@ -1,38 +1,34 @@
 #include "./monitor.h"
-#include <assert.h>
+#include <cassert>
 #include <thread>
 #include <chrono>
 #include <iostream>
+#include <vector>
 using std::cout, std::flush, std::this_thread::sleep_for, std::chrono::seconds;
 
-int vitalsOk(float temperature, float pulseRate, float spo2) {
-  if (temperature > 102 || temperature < 95) {
-    cout << "Temperature is critical!\n";
+VitalRange checks[] = {
+    {Temperature,95.0f, 102.0f, "Temperature is critical!\n"},
+    {PulseRate, 60.0f, 100.0f, "Pulse Rate is out of range!\n"},
+    {SPO2, 90.0f, std::numeric_limits<float>::max(), "Oxygen Saturation out of range!\n"}
+};
+
+void showMessage(std::string message) {
+    cout << message;
     for (int i = 0; i < 6; i++) {
-      cout << "\r* " << flush;
-      sleep_for(seconds(1));
-      cout << "\r *" << flush;
-      sleep_for(seconds(1));
-    }
-    return 0;
-  } else if (pulseRate < 60 || pulseRate > 100) {
-    cout << "Pulse Rate is out of range!\n";
-    for (int i = 0; i < 6; i++) {
-      cout << "\r* " << flush;
-      sleep_for(seconds(1));
-      cout << "\r *" << flush;
-      sleep_for(seconds(1));
-    }
-    return 0;
-  } else if (spo2 < 90) {
-    cout << "Oxygen Saturation out of range!\n";
-    for (int i = 0; i < 6; i++) {
-      cout << "\r* " << flush;
-      sleep_for(seconds(1));
-      cout << "\r *" << flush;
-      sleep_for(seconds(1));
-    }
-    return 0;
+    cout << "\r* " << flush;
+    sleep_for(seconds(1));
+    cout << "\r *" << flush;
+    sleep_for(seconds(1));
   }
-  return 1;
+}
+
+int vitalsOk(std::vector<VitalValue> inputVitals) {
+    for (size_t i = 0; i < inputVitals.size(); i++) {
+        assert(inputVitals[i].vital == checks[i].vital);
+        if (inputVitals[i].value < checks[i].min || inputVitals[i].value > checks[i].max) {
+            showMessage(checks[i].message);
+            return 0;
+        }
+    }
+    return 1;
 }
--- test-monitor.cpp
+++ test-monitor.cpp
@@ -2,7 +2,68 @@
 
 #include "gtest/gtest.h"
 
-TEST(Monitor, NotOkWhenAnyVitalIsOffRange) {
-  ASSERT_FALSE(vitalsOk(99, 102, 70));
-  ASSERT_TRUE(vitalsOk(98.1, 70, 98));
+TEST(Monitor, HighTemperature)
+{
+    std::vector<VitalValue> measuredValues = {
+        {Temperature, 103},
+        {PulseRate, 70},
+        {SPO2, 95}
+    };
+
+  ASSERT_FALSE(vitalsOk(measuredValues));
+}
+
+TEST(Monitor, LowTemperature)
+{
+    std::vector<VitalValue> measuredValues = {
+        {Temperature, 90},
+        {PulseRate, 70},
+        {SPO2, 95}
+    };
+
+    ASSERT_FALSE(vitalsOk(measuredValues));
+}
+
+TEST(Monitor, HighPulseRate)
+{
+    std::vector<VitalValue> measuredValues = {
+        {Temperature, 96},
+        {PulseRate, 120},
+        {SPO2, 95}
+    };
+
+    ASSERT_FALSE(vitalsOk(measuredValues));
+}
+
+TEST(Monitor, LowPulseRate)
+{
+    std::vector<VitalValue> measuredValues = {
+        {Temperature, 96},
+        {PulseRate, 40},
+        {SPO2, 95}
+    };
+
+    ASSERT_FALSE(vitalsOk(measuredValues));
+}
+
+TEST(Monitor, LowSPO2)
+{
+    std::vector<VitalValue> measuredValues = {
+        {Temperature, 96},
+        {PulseRate, 70},
+        {SPO2, 70}
+    };
+
+    ASSERT_FALSE(vitalsOk(measuredValues));
+}
+
+TEST(Monitor, AllVitalsOK)
+{
+    std::vector<VitalValue> measuredValues = {
+        {Temperature, 96},
+        {PulseRate, 70},
+        {SPO2, 95}
+    };
+
+    ASSERT_TRUE(vitalsOk(measuredValues));
 }
